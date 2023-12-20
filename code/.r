0c8fff3b19be4b0ac0ebaa0cb469e7175e5de9ba
-U <-U>
2023-12-20 17:52:27 -0300

    funcionando

--- code/.r
+++ code/.r
@@ -1,11 +1,16 @@
-install.packages("xaringan")
+# install.packages("xaringan")
 
-install.packages("installr")
-library(installr)
-installr::install.pandoc()
-
-
-rmarkdown::pandoc_available()
+# install.packages("installr")
+# library(installr)
+# installr::install.pandoc()
 
 
+# rmarkdown::pandoc_available()
+
+
+# RENDERIZAR O ARQUIVOS
 rmarkdown::render("code/slidecode.Rmd")
+
+
+# site:
+#   http://localhost:5500/code/slidecode.html#1
